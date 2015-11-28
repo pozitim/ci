c15a33e9a240a76247a628331449457979ee36ce
Ömer ÜCEL <omerucel@gmail.com>
2015-11-28 22:37:45 +0200

    Services ayarı kullanılmadığında docker-compose links parametresinin kullanılmaması sağlandı. Parser iyileştirildi.

--- src/Pozitim/CI/Docker/Compose/Service/WebServiceParser.php
+++ src/Pozitim/CI/Docker/Compose/Service/WebServiceParser.php
@@ -10,20 +10,20 @@ class WebServiceParser implements ServiceParser
      */
     public function parse(array $suiteConfigs)
     {
-        return [
-            'image' => 'pozitim-ci/' . $suiteConfigs['image'],
-            'environment' => $this->prepareEnvironments($suiteConfigs),
-            'links' => $this->prepareLinks($suiteConfigs),
-            'volumes' => $this->prepareVolumes($suiteConfigs),
-            'command' => $this->prepareCommand($suiteConfigs)
-        ];
+        $serviceConfig = ['image' => 'pozitim-ci/' . $suiteConfigs['image']];
+        $serviceConfig = $this->prepareEnvironments($serviceConfig, $suiteConfigs);
+        $serviceConfig = $this->prepareVolumes($serviceConfig, $suiteConfigs);
+        $serviceConfig = $this->prepareCommand($serviceConfig, $suiteConfigs);
+        $serviceConfig = $this->prepareLinks($serviceConfig, $suiteConfigs);
+        return $serviceConfig;
     }
 
     /**
+     * @param array $serviceConfig
      * @param array $suiteConfigs
      * @return array
      */
-    public function prepareEnvironments(array $suiteConfigs = [])
+    public function prepareEnvironments(array $serviceConfig, array $suiteConfigs = [])
     {
         $publicFolder = '/project/public';
         $indexFile = 'index.php';
@@ -33,43 +33,51 @@ class WebServiceParser implements ServiceParser
         if (isset($suiteConfigs['services']['web']['index_file'])) {
             $indexFile = $suiteConfigs['services']['web']['index_file'];
         }
-        $environments = [
-            'NGINX_PUBLIC_FOLDER' => $publicFolder,
-            'NGINX_INDEX_FILE' => $indexFile
-        ];
-        return array_merge($environments, isset($suiteConfigs['env']) ? $suiteConfigs['env'] : []);
+        $env = isset($suiteConfigs['env']) ? $suiteConfigs['env'] : [];
+        $env['NGINX_PUBLIC_FOLDER'] = $publicFolder;
+        $env['NGINX_INDEX_FILE'] = $indexFile;
+        $serviceConfig['environment'] = $env;
+        return $serviceConfig;
     }
 
     /**
+     * @param array $serviceConfig
      * @param array $suiteConfigs
      * @return array
      */
-    public function prepareLinks(array $suiteConfigs = [])
+    public function prepareLinks(array $serviceConfig, array $suiteConfigs = [])
     {
         $links = [];
-        foreach ($suiteConfigs['services'] as $serviceName => $serviceValues) {
+        $services = isset($suiteConfigs['services']) ? $suiteConfigs['services'] : [];
+        foreach ($services as $serviceName => $serviceValues) {
             if ($serviceName == 'web') {
                 continue;
             }
             $links[] = $serviceName . ':' . $serviceName;
         }
-        return $links;
+        if (!empty($links)) {
+            $serviceConfig['links'] = $links;
+        }
+        return $serviceConfig;
     }
 
     /**
+     * @param array $serviceConfig
      * @param array $suiteConfigs
      * @return array
      */
-    public function prepareVolumes(array $suiteConfigs = [])
+    public function prepareVolumes(array $serviceConfig, array $suiteConfigs = [])
     {
-        return ['./source-code:/project'];
+        $serviceConfig['volumes'] = ['./source-code:/project'];
+        return $serviceConfig;
     }
 
     /**
+     * @param array $serviceConfig
      * @param array $suiteConfigs
      * @return array
      */
-    public function prepareCommand(array $suiteConfigs = [])
+    public function prepareCommand(array $serviceConfig, array $suiteConfigs = [])
     {
         $commands = [
             'sed -i -e "s#<<public_folder>>#${NGINX_PUBLIC_FOLDER}#g" /etc/nginx/nginx.conf',
@@ -77,6 +85,8 @@ class WebServiceParser implements ServiceParser
             '/usr/sbin/php-fpm -R',
             '/usr/sbin/nginx'
         ];
-        return array_merge($commands, $suiteConfigs['scripts']);
+        $commands = array_merge($commands, $suiteConfigs['scripts']);
+        $serviceConfig['command'] = $commands;
+        return $serviceConfig;
     }
 }
--- tests/unit/Pozitim/CI/Docker/Compose/Service/WebServiceParserTest.php
+++ tests/unit/Pozitim/CI/Docker/Compose/Service/WebServiceParserTest.php
@@ -40,57 +40,98 @@ class WebServiceParserTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($expected, $parsed);
     }
 
+    public function testEmptyLinks()
+    {
+        $parser = new WebServiceParser();
+        $expected = [
+            'image' => 'pozitim-ci/centos-php54',
+            'environment' => [
+                'APPLICATION_ENV' => 'pozitim-ci',
+                'NGINX_PUBLIC_FOLDER' => '/project/public',
+                'NGINX_INDEX_FILE' => 'index.php'
+            ],
+            'volumes' => [
+                './source-code:/project'
+            ],
+            'command' => [
+                'sed -i -e "s#<<public_folder>>#${NGINX_PUBLIC_FOLDER}#g" /etc/nginx/nginx.conf',
+                'sed -i -e "s#<<index_file>>#${NGINX_INDEX_FILE}#g" /etc/nginx/nginx.conf',
+                '/usr/sbin/php-fpm -R',
+                '/usr/sbin/nginx',
+                'sh /project/tests/pozitim-ci/install.sh',
+                'sh /project/tests/pozitim-ci/run.sh'
+            ]
+        ];
+        $suiteConfigs = [
+            'image' => 'centos-php54',
+            'env' => ['APPLICATION_ENV' => 'pozitim-ci'],
+            'scripts' => ['sh /project/tests/pozitim-ci/install.sh', 'sh /project/tests/pozitim-ci/run.sh']
+        ];
+        $parsed = $parser->parse($suiteConfigs);
+        $this->assertEquals($expected, $parsed);
+    }
+
     public function testDefaultNginxEnv()
     {
         $parser = new WebServiceParser();
         $expected = [
-            'NGINX_PUBLIC_FOLDER' => '/project/public',
-            'NGINX_INDEX_FILE' => 'index.php'
+            'environment' => [
+                'NGINX_PUBLIC_FOLDER' => '/project/public',
+                'NGINX_INDEX_FILE' => 'index.php'
+            ]
         ];
-        $parsed = $parser->prepareEnvironments();
-        $this->assertEquals($expected, $parsed);
+        $serviceConfigs = $parser->prepareEnvironments([], []);
+        $this->assertEquals($expected, $serviceConfigs);
     }
 
     public function testSpecialEnv()
     {
         $parser = new WebServiceParser();
         $expected = [
-            'APPLICATION_ENV' => 'pozitim-ci',
-            'NGINX_PUBLIC_FOLDER' => '/project/public',
-            'NGINX_INDEX_FILE' => 'index.php'
+            'environment' => [
+                'APPLICATION_ENV' => 'pozitim-ci',
+                'NGINX_PUBLIC_FOLDER' => '/project/public',
+                'NGINX_INDEX_FILE' => 'index.php'
+            ]
         ];
-        $parsed = $parser->prepareEnvironments([
+        $suiteConfigs = [
             'env' => ['APPLICATION_ENV' => 'pozitim-ci']
-        ]);
-        $this->assertEquals($expected, $parsed);
+        ];
+        $serviceConfigs = $parser->prepareEnvironments([], $suiteConfigs);
+        $this->assertEquals($expected, $serviceConfigs);
     }
 
     public function testOverrideNginxEnv()
     {
         $parser = new WebServiceParser();
         $expected = [
-            'NGINX_PUBLIC_FOLDER' => '/project/admin',
-            'NGINX_INDEX_FILE' => 'api.php'
+            'environment' => [
+                'NGINX_PUBLIC_FOLDER' => '/project/admin',
+                'NGINX_INDEX_FILE' => 'api.php'
+            ]
         ];
-        $parsed = $parser->prepareEnvironments([
+        $suiteConfigs = [
             'services' => [
                 'web' => [
                     'public_folder' => '/project/admin',
                     'index_file' => 'api.php'
                 ]
             ]
-        ]);
-        $this->assertEquals($expected, $parsed);
+        ];
+        $serviceConfigs = $parser->prepareEnvironments([], $suiteConfigs);
+        $this->assertEquals($expected, $serviceConfigs);
     }
 
     public function testPrepareLinks()
     {
         $parser = new WebServiceParser();
         $expected = [
-            'gearmand:gearmand',
-            'redis:redis'
+            'links' => [
+                'gearmand:gearmand',
+                'redis:redis'
+            ]
         ];
-        $parsed = $parser->prepareLinks([
+        $suiteConfigs = [
             'services' => [
                 'gearmand' => null,
                 'redis' => null,
@@ -99,29 +140,33 @@ class WebServiceParserTest extends \PHPUnit_Framework_TestCase
                     'index_file' => 'api.php'
                 ]
             ]
-        ]);
-        $this->assertEquals($expected, $parsed);
+        ];
+        $serviceConfig = $parser->prepareLinks([], $suiteConfigs);
+        $this->assertEquals($expected, $serviceConfig);
     }
 
     public function testPrepareVolumes()
     {
         $parser = new WebServiceParser();
-        $expected = ['./source-code:/project'];
-        $parsed = $parser->prepareVolumes();
-        $this->assertEquals($expected, $parsed);
+        $expected = ['volumes' => ['./source-code:/project']];
+        $serviceConfigs = $parser->prepareVolumes([], []);
+        $this->assertEquals($expected, $serviceConfigs);
     }
 
     public function testPrepareCommand()
     {
         $parser = new WebServiceParser();
         $expected = [
-            'sed -i -e "s#<<public_folder>>#${NGINX_PUBLIC_FOLDER}#g" /etc/nginx/nginx.conf',
-            'sed -i -e "s#<<index_file>>#${NGINX_INDEX_FILE}#g" /etc/nginx/nginx.conf',
-            '/usr/sbin/php-fpm -R',
-            '/usr/sbin/nginx',
-            'sh /project/tests/pozitim-ci/run-tests.sh'
+            'command' => [
+                'sed -i -e "s#<<public_folder>>#${NGINX_PUBLIC_FOLDER}#g" /etc/nginx/nginx.conf',
+                'sed -i -e "s#<<index_file>>#${NGINX_INDEX_FILE}#g" /etc/nginx/nginx.conf',
+                '/usr/sbin/php-fpm -R',
+                '/usr/sbin/nginx',
+                'sh /project/tests/pozitim-ci/run-tests.sh'
+            ]
         ];
-        $parsed = $parser->prepareCommand(['scripts' => ['sh /project/tests/pozitim-ci/run-tests.sh']]);
+        $suiteConfigs = ['scripts' => ['sh /project/tests/pozitim-ci/run-tests.sh']];
+        $parsed = $parser->prepareCommand([], $suiteConfigs);
         $this->assertEquals($expected, $parsed);
     }
 }
