d785e0ea3378ce6ec2200cfb04fd55a8fc75d44f
Ömer ÜCEL <omerucel@gmail.com>
2016-03-06 18:38:59 +0800

    php54 docker desteği.

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -5,6 +5,7 @@ docker pull memcached
 docker pull redis
 docker pull mongo
 docker pull pataquest/gearmand
+docker build -t pozitim-ci/centos-php53 centos-php53/
 docker build -t pozitim-ci/centos-php54 centos-php54/
 docker build -t pozitim-ci/centos-php55 centos-php55/
 docker build -t pozitim-ci/centos-php56 centos-php56/
